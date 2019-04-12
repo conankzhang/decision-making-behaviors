ac488f091cf6721ae042804fb48dd401e90127d1
conankzhang <conankzhang@gmail.com>
2019-04-11 23:38:20 -0600

    Tick: Use initializer list for members

--- src/BehaviorTree/Tick.cpp
+++ src/BehaviorTree/Tick.cpp
@@ -1,7 +1,9 @@
 #include "Tick.h"
 
 //=======================================================================================================================
-CTick::CTick(CBehaviorTree* InBehaviorTree, CBlackBoard* InBlackBoard)
+CTick::CTick(CBehaviorTree* InBehaviorTree, CBlackBoard* InBlackBoard) :
+	BehaviorTree(InBehaviorTree),
+	BlackBoard(InBlackBoard)
 {
 
 }
